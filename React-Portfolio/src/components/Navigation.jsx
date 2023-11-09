6c0773c6dd6667d2cf4e2e874051d849f2cce02f
jhuber1155 <141798215+jhuber1155@users.noreply.github.com>
2023-11-08 22:13:18 -0800

    Added an bootstrap image to project to call in portfolio and added Name to navbar

--- React-Portfolio/src/components/Navigation.jsx
+++ React-Portfolio/src/components/Navigation.jsx
@@ -6,6 +6,7 @@ function CollapsibleExample() {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
+      <Navbar.Brand>John Huber</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
