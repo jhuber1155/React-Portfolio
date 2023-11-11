bc108f25574cbf67cabd07df99f609c189e91fb2
jhuber1155 <141798215+jhuber1155@users.noreply.github.com>
2023-11-11 14:16:23 -0800

    Coded contact form validators and logic

--- React-Portfolio/src/pages/Contact.jsx
+++ React-Portfolio/src/pages/Contact.jsx
@@ -1,24 +1,85 @@
+import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { checkText, validateEmail } from '../utils/helpers';
 
-function TextControlsExample() {
+function ContactForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [text, setText] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'name') {
+      setName(inputValue);
+    } else {
+      setText(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email) || !name) {
+      setErrorMessage('Please enter valid text');
+      return;
+    }
+
+    if (!checkText(text)) {
+      setErrorMessage(
+        `Please don't leave this text area blank when submitting a message`
+      );
+      return;
+    }
+
+    setName('');
+    setText('');
+    setEmail('');
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleFormSubmit}>
       <Form.Group className="mb-3" controlId="contact-name">
         <Form.Label>Name:</Form.Label>
-        <Form.Control type="text" placeholder="Name" />
+        <Form.Control
+          onChange={handleInputChange}
+          type="text"
+          name="name"
+          placeholder="Name"
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="contact-email">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
+        <Form.Control
+          onChange={handleInputChange}
+          type="email"
+          name="email"
+          placeholder="name@example.com"
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="contact-text-area">
         <Form.Label>Please Enter your Comments Here</Form.Label>
-        <Form.Control as="textarea" rows={3} />
+        <Form.Control
+          onChange={handleInputChange}
+          as="textarea"
+          rows={3}
+          name="text"
+        />
       </Form.Group>
       <Button as="input" type="submit" value="Submit" />{' '}
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </Form>
   );
 }
 
-export default TextControlsExample;
+export default ContactForm;
--- React-Portfolio/src/utils/helpers.js
+++ React-Portfolio/src/utils/helpers.js
@@ -2,3 +2,11 @@ export function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   }
+
+  export function checkText(input) {
+    const text = /^.+$/;
+    if (input.match(text)) {
+      return true;
+    }
+    return false;
+  }
